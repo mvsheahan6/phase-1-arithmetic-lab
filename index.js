0d5c80a245ff4a752e5a65119c4e2f4e863aed98
Matthew Sheahan <sheahan.matt@gmail.com>
2022-11-20 13:54:39 -0800

    commit tree b96282724f761a840205c37ba8d2c6284ae08e35

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+const num3 = 36;
+const num4 = 8;
+
+let mod = num3 % num4;
+
+let max = Math.max(12, 5, 20, 18, 11);
